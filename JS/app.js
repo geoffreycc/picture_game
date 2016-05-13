e70059aeb36cd115cbb0601bf27832c2f326301b
geoffreycc <gcox3d@gmail.com>
2016-05-12 21:58:30 -0700

    working on display image method. Not working.

--- JS/app.js
+++ JS/app.js
@@ -6,6 +6,7 @@ var productName = ['bag', 'banana', 'boots', 'chair', 'cthulhu', 'dragon', 'pen'
 function Product(name) {
   this.name = name;
   this.voteCount = 0;
+  this.path = 'images/' + name + '.jpg';
   allProducts.push(this);
 };
 
@@ -17,15 +18,29 @@ function productMaker(arr) { // For loop and function call to create new instanc
 productMaker(productName);
 
 var productRanker = {
-  getRandomIndex: function(arr){ //generate a random number between 0 and 13
-    var ranIndex = Math.floor(Math.random() * arr);
+  getRandomIndex: function(arr) { //generate a random number between 0 and 13
+    var ranIndex = Math.floor(Math.random() * arr.length);
     console.log(ranIndex);
   },
-  displayImages: function(){}, //render three images to page
-  tallyClicks: function(){}, //keep track of each image's number of votes
-  displayResults: function(){}, //After 15? clicks render the results as a list with 3 columns
-  showButton: function(){}, //display the results button or the reset button
-  onClick: function(){}, //drives what happens on click
+  displayImages: function() { //render three different images to page
+    var imageDiv = document.getElementById('images');
+    for (var i = 0; i < 3; i++) {
+      var imgEl = document.createElement('img');
+      var num = productRanker.getRandomIndex(allProducts);
+      console.log(num);
+      imgEl.src = allProducts[num].path.value; //not working
+      console.log(imgEl);
+      imageDiv.appendChild(imgEl);
+    }
+  },
+  tallyClicks: function() { //keep track of number of votes
+  },
+  displayResults: function() { // After 15? clicks show button to render the results list in three columns
+  },
+  showButton: function() { //display the results button or the reset button
+  },
+  onClick: function() { //drives the events that occur when a click happens
+  },
 };
 
 // productRanker.(some variable representing an html tag).addEventListener('click', productRanker.onClick);
