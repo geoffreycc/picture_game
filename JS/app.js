00fe2756e35770bf5395fe559d7c25c7fb0a4a20
geoffreycc <gcox3d@gmail.com>
2016-05-12 21:03:16 -0700

    Created method to generate a random number.

--- JS/app.js
+++ JS/app.js
@@ -9,16 +9,24 @@ function Product(name) {
   allProducts.push(this);
 };
 
-function productMaker(arr) { //                   For loop and function call to create new instances
+function productMaker(arr) { // For loop and function call to create new instances
   for (var p = 0; p < arr.length; p++){
     var newProduct = new Product(arr[p]);
   }
 }
 productMaker(productName);
 
-var voteTracker = {
+var productRanker = {
+  getRandomIndex: function(arr){ //generate a random number between 0 and 13
+    var ranIndex = Math.floor(Math.random() * arr);
+    console.log(ranIndex);
+  },
+  displayImages: function(){}, //render three images to page
+  tallyClicks: function(){}, //keep track of each image's number of votes
+  displayResults: function(){}, //After 15? clicks render the results as a list with 3 columns
+  showButton: function(){}, //display the results button or the reset button
+  onClick: function(){}, //drives what happens on click
 };
 
-// var bag = new Picture ('bag'); // Testing object constructor
-
-// document.getElementById('pic1').src = 'images/' + something + '.jpg'; // Testing image
+// productRanker.(some variable representing an html tag).addEventListener('click', productRanker.onClick);
+// productRanker.displayImages();
