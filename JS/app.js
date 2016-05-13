710258ec84866aaa3a595490b67c13f87bcac4de
geoffreycc <gcox3d@gmail.com>
2016-05-12 20:20:47 -0700

    Created for loop to make new instances of Product object.

--- JS/app.js
+++ JS/app.js
@@ -1,12 +1,24 @@
+// img Object constructor, tracker Object literal, event handler
+
 var allProducts = [];
 var productName = ['bag', 'banana', 'boots', 'chair', 'cthulhu', 'dragon', 'pen', 'scissors', 'shark', 'sweep', 'unicorn', 'usb', 'water_can', 'wine_glass'];
 
-function Picture (name) {
+function Product(name) {
   this.name = name;
   this.voteCount = 0;
   allProducts.push(this);
 };
 
-var bag = new Picture ('bag');
+function productMaker(arr) { //                   For loop and function call to create new instances
+  for (var p = 0; p < arr.length; p++){
+    var newProduct = new Product(arr[p]);
+  }
+}
+productMaker(productName);
 
-document.getElementById('pic1').src = 'images/' + bag.name + '.jpg';
+var voteTracker = {
+};
+
+// var bag = new Picture ('bag'); // Testing object constructor
+
+// document.getElementById('pic1').src = 'images/' + something + '.jpg'; // Testing image
