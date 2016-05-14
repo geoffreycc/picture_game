70eb6baaa50bd86c2b346036cb189c22879668e5
geoffreycc <gcox3d@gmail.com>
2016-05-14 13:43:28 -0700

    Method to render three different random images to page functioning.

--- JS/app.js
+++ JS/app.js
@@ -10,7 +10,7 @@ function Product(name) {
   allProducts.push(this);
 };
 
-function productMaker(arr) { // For loop and function call to create new instances
+function productMaker(arr) { // For loop and function call to create new instances  TODO Rework this function into an iffy
   for (var p = 0; p < arr.length; p++){
     var newProduct = new Product(arr[p]);
   }
@@ -18,20 +18,35 @@ function productMaker(arr) { // For loop and function call to create new instanc
 productMaker(productName);
 
 var productRanker = {
+  imageSec: document.getElementById('images'),  //May want to declare a bunch of stuff here. Have not been able to get this to work
+  ranIndex: 0,
   getRandomIndex: function(arr) { //generate a random number between 0 and 13
-    var ranIndex = Math.floor(Math.random() * arr.length);
-    console.log(ranIndex);
+    return Math.floor(Math.random() * arr.length);
+    // console.log('The random number is ' + ranIndex);
   },
+
   displayImages: function() { //render three different images to page
     var imageDiv = document.getElementById('images');
-    for (var i = 0; i < 3; i++) {
-      var imgEl = document.createElement('img');
-      var num = productRanker.getRandomIndex(allProducts);
-      console.log(num);
-      imgEl.src = allProducts[num].path.value; //not working
-      console.log(imgEl);
-      imageDiv.appendChild(imgEl);
+    var img1 = document.getElementById('pic1');
+    var img2 = document.getElementById('pic2');
+    var img3 = document.getElementById('pic3');
+    // for (var i = 0; i < 3; i++) {
+    img1.src = allProducts[this.getRandomIndex(allProducts)].path;
+    console.log(img1.src);
+    img2.src = allProducts[this.getRandomIndex(allProducts)].path;
+    console.log(img2.src);
+    img3.src = allProducts[this.getRandomIndex(allProducts)].path;
+    console.log(img3.src);
+    if (img1.src === img2.src || img1.src === img3.src || img2.src === img3.src) {
+      this.displayImages();
     }
+      // var imgEl = document.createElement('img');
+      // var num = productRanker.getRandomIndex(allProducts);
+      // console.log(ranIndex);
+      // imgEl.src = allProducts[ranIndex].path; //not working
+      // console.log(imgEl);
+      // imageDiv.appendChild(imgEl);
+    // }
   },
   tallyClicks: function() { //keep track of number of votes
   },
