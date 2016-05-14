f4a17fcaff9d53f4ca49b9485cabf5b435f5a132
geoffreycc <gcox3d@gmail.com>
2016-05-14 16:02:46 -0700

    Count for each picture is increased when clicked. Total count goes up by 1 each click.

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,3 @@
-// img Object constructor, tracker Object literal, event handler
-
 var allProducts = [];
 var productName = ['bag', 'banana', 'boots', 'chair', 'cthulhu', 'dragon', 'pen', 'scissors', 'shark', 'sweep', 'unicorn', 'usb', 'water_can', 'wine_glass'];
 
@@ -19,44 +17,55 @@ productMaker(productName);
 
 var productRanker = {
   imageSec: document.getElementById('images'),  //May want to declare a bunch of stuff here. Have not been able to get this to work
-  ranIndex: 0,
+  img1: document.getElementById('pic1'),
+  img2: document.getElementById('pic2'),
+  img3: document.getElementById('pic3'),
+  timeRun: 0,
   getRandomIndex: function(arr) { //generate a random number between 0 and 13
     return Math.floor(Math.random() * arr.length);
-    // console.log('The random number is ' + ranIndex);
   },
-
   displayImages: function() { //render three different images to page
-    var imageDiv = document.getElementById('images');
-    var img1 = document.getElementById('pic1');
-    var img2 = document.getElementById('pic2');
-    var img3 = document.getElementById('pic3');
-    // for (var i = 0; i < 3; i++) {
-    img1.src = allProducts[this.getRandomIndex(allProducts)].path;
-    console.log(img1.src);
-    img2.src = allProducts[this.getRandomIndex(allProducts)].path;
-    console.log(img2.src);
-    img3.src = allProducts[this.getRandomIndex(allProducts)].path;
-    console.log(img3.src);
-    if (img1.src === img2.src || img1.src === img3.src || img2.src === img3.src) {
+    var num1 = this.getRandomIndex(allProducts);
+    var num2 = this.getRandomIndex(allProducts);
+    var num3 = this.getRandomIndex(allProducts);
+    console.log('num1 is ' + num1);
+    this.img1.src = allProducts[num1].path; // TODO trim down this junk.
+    this.img1.name = allProducts[num1].name;
+    this.img2.src = allProducts[num2].path;
+    this.img2.name = allProducts[num2].name;
+    this.img3.src = allProducts[num3].path;
+    this.img3.name = allProducts[num3].name;
+    if (this.img1.src === this.img2.src || this.img1.src === this.img3.src || this.img2.src === this.img3.src) {
       this.displayImages();
     }
-      // var imgEl = document.createElement('img');
-      // var num = productRanker.getRandomIndex(allProducts);
-      // console.log(ranIndex);
-      // imgEl.src = allProducts[ranIndex].path; //not working
-      // console.log(imgEl);
-      // imageDiv.appendChild(imgEl);
-    // }
   },
-  tallyClicks: function() { //keep track of number of votes
+  tallyClicks: function() { //keep track of number of votes on the instance's voteCount and on timesRun
+    this.timeRun ++;
+    for (var i = 0; i < allProducts.length; i ++) {
+      if (event.target.name === allProducts[i].name) {
+        allProducts[i].voteCount ++;
+        console.log(allProducts[i].voteCount + ' is the voteCount for ' + allProducts[i]);
+      }
+    }
+    // allProducts[event.target.src]
   },
   displayResults: function() { // After 15? clicks show button to render the results list in three columns
   },
   showButton: function() { //display the results button or the reset button
+    if (this.timeRun === 15) {
+      //Show results Button
+    }
   },
   onClick: function() { //drives the events that occur when a click happens
   },
 };
-
 // productRanker.(some variable representing an html tag).addEventListener('click', productRanker.onClick);
 // productRanker.displayImages();
+productRanker.displayImages();
+productRanker.imageSec.addEventListener('click', function (event) {
+  event.preventDefault();
+  console.log('The event target name is ' + event.target.name);
+  productRanker.tallyClicks();
+  console.log('The timeRun is ' + productRanker.timeRun);
+  // console.log(event.target.src);
+});
